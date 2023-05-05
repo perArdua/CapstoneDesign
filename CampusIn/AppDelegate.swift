42f691accf892cb1be6984305664e01e2e4d5a77
ekrud99 <201810999@sangmyung.kr>
2023-05-06 00:33:24 +0900

    Feat. 로그인 여부 식별 기능 구현

--- CampusIn/AppDelegate.swift
+++ CampusIn/AppDelegate.swift
@@ -1,10 +1,9 @@
 //
 //  AppDelegate.swift
-//  CampusIn
+//  CapstonDesign
 //
-//  Created by 이동현 on 2023/05/03.
+//  Created by 최다경 on 2023/04/02.
 //
-
 import UIKit
 
 @main
@@ -14,11 +13,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let defaults = UserDefaults.standard
+        print("app delegate \(defaults.value(forKey: "isLoggedIn"))")
+        // 로그인 상태에 따라 로그인 화면으로 갈지 메인화면으로 갈지 분류
+        if let loginCheck = defaults.value(forKey: "isLoggedIn") {
+            if loginCheck as! Bool {
+                // 이미 로그인 되어있는 경우
+                print("login status: true")
+            } else {
+                // 로그인 되지 않은 경우
+                print("login status: false")
+                defaults.set(false, forKey: "isLoggedIn") // 로그인 여부에 false 저장
+            }
+        } else{
+            //신규 유저의 경우
+            print("nwe user")
+            defaults.set(false, forKey: "isLoggedIn") // 로그인 여부에 false 저장
+        }
+        
+       
+        
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -33,4 +52,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
