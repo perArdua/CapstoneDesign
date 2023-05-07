c218ac7aa6d4ab5e8f1f4e38bf12ec84fcccf1d9
Kim Tae Ho <kok8454@gmail.com>
2023-05-08 06:04:20 +0900

    Feat. post관련 entity 및 dto 작성

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/post/Post.java
@@ -0,0 +1,93 @@
+package com.example.campusin.domain.post;
+
+import com.example.campusin.domain.basetime.BaseTimeEntity;
+import com.example.campusin.domain.board.Board;
+import com.example.campusin.domain.photo.Photo;
+import com.example.campusin.domain.post.dto.request.PostUpdateRequest;
+import com.example.campusin.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kok8454@gmail.com on 2023-05-07
+ * Github : http://github.com/perArdua
+ */
+
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE post SET deleted_at = CURRENT_TIMESTAMP where id = ?")
+@Getter
+@NoArgsConstructor
+@Entity
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "content", length = 65535)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", referencedColumnName = "board_id")
+    private Board board;
+
+//    @OneToMany(mappedBy = "post")
+//    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Photo> photos = new ArrayList<>();
+
+    private Integer commentCount = 0;
+    private Integer likeCount = 0;
+
+
+    @Builder
+    public Post(String title, String content, User user, Board board) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.board = board;
+    }
+
+    public void updatePost(PostUpdateRequest request) {
+        setTitle(request.getTitle());
+        setContent(request.getContent());
+        setPhotos(request.getPhotos());
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public void addPhoto(final Photo photo) {
+        photos.add(photo);
+        photo.setPost(this);
+    }
+
+    public void removePhoto(final Photo photo) {
+        photos.remove(photo);
+        photo.setPost(null);
+    }
+}
---
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/post/dto/response/PostSimpleResponse.java
@@ -0,0 +1,44 @@
+package com.example.campusin.domain.post.dto.response;
+
+import com.example.campusin.domain.board.dto.response.BoardSimpleResponse;
+import com.example.campusin.domain.post.Post;
+import lombok.Builder;
+
+/**
+ * Created by kok8454@gmail.com on 2023-05-07
+ * Github : http://github.com/perArdua
+ */
+
+
+
+public class PostSimpleResponse {
+    Long postId;
+    BoardSimpleResponse boardSimpleResponse;
+    Long userId;
+    String writer;
+    String title;
+    String content;
+
+
+    @Builder
+    public PostSimpleResponse(Long postId, BoardSimpleResponse boardSimpleResponse, Long userId, String writer, String title, String content) {
+        this.postId = postId;
+        this.boardSimpleResponse = boardSimpleResponse;
+        this.userId = userId;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Builder
+    public PostSimpleResponse(Post entity) {
+        this(
+                entity.getId(),
+                new BoardSimpleResponse(entity.getBoard()),
+                entity.getUser().getUserSeq(),
+                entity.getUser().getUserId(),
+                entity.getTitle(),
+                entity.getContent()
+        );
+    }
+}
