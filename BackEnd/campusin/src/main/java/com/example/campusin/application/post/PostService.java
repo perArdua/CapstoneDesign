78a414b153d4ff7b380c1543bb3ed6c11a1c19c4
Kim Tae Ho <kok8454@gmail.com>
2023-05-08 06:26:53 +0900

    Feat. post service 작성

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/application/post/PostService.java
@@ -0,0 +1,111 @@
+package com.example.campusin.application.post;
+
+import com.example.campusin.domain.board.Board;
+import com.example.campusin.domain.board.dto.response.BoardSimpleResponse;
+import com.example.campusin.domain.photo.Photo;
+import com.example.campusin.domain.post.Post;
+import com.example.campusin.domain.post.dto.request.PostCreateRequest;
+import com.example.campusin.domain.post.dto.request.PostUpdateRequest;
+import com.example.campusin.domain.post.dto.response.PostIdResponse;
+import com.example.campusin.domain.post.dto.response.PostResponse;
+import com.example.campusin.domain.post.dto.response.PostSimpleResponse;
+import com.example.campusin.domain.user.User;
+import com.example.campusin.infra.board.BoardRepository;
+import com.example.campusin.infra.photo.PhotoRepository;
+import com.example.campusin.infra.post.PostRepository;
+import com.example.campusin.infra.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+/**
+ * Created by kok8454@gmail.com on 2023-03-19
+ * Github : http://github.com/perArdua
+ */
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+    private final PhotoRepository photoRepository;
+
+    @Transactional(readOnly = true)
+    public Page<PostSimpleResponse> getPostsByBoard(Long boardId, Pageable pageable) {
+        findBoard(boardId);
+        Page<Post> posts = postRepository.findPostsByBoardId(boardId, pageable);
+        return posts.map(PostSimpleResponse::new);
+    }
+
+    @Transactional
+    public PostIdResponse createPost(Long boardId, Long userId, PostCreateRequest request) {
+        Board board = findBoard(boardId);
+        User user = findUser(userId);
+        Post post = postRepository.save(Post.builder()
+                .board(board)
+                .user(user)
+                .title(request.getTitle())
+                .content(request.getContent())
+                .build());
+        for (String content : request.getPhotos()) {
+            Photo savePhoto = new Photo(content);
+            savePhoto.setPost(post);
+            photoRepository.save(savePhoto);
+        }
+        return new PostIdResponse(postRepository.save(post).getId());
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponse readPost(Long postId) {
+        Post post = findPost(postId);
+        return new PostResponse(post);
+    }
+
+
+    @Transactional
+    public PostIdResponse updatePost(Long postId, PostUpdateRequest request) {
+        Post post = findPost(postId);
+        post.updatePost(request);
+        return new PostIdResponse(postRepository.save(post).getId());
+    }
+    @Transactional
+    public void deletePost(Long postId) {
+        Post post = findPost(postId);
+        postRepository.deleteById(postId);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<PostSimpleResponse> searchPosts(String keyword, Pageable pageable) {
+        return postRepository.searchPosts(keyword, pageable).map(PostSimpleResponse::new);
+    }
+
+    public Page<PostSimpleResponse> searchPostsAtBoard(Long boardId, String keyword, Pageable pageable) {
+        return postRepository.searchPostsAtBoard(keyword, boardId, pageable)
+                .map(PostSimpleResponse::new);
+    }
+
+    private Board findBoard(Long boardId) {
+        return boardRepository.findById(boardId).orElseThrow(
+                () -> new IllegalArgumentException("BOARD NOT FOUND")
+        );
+    }
+
+    private Post findPost(Long postId) {
+        return postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("POST NOT FOUND")
+        );
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("USER NOT FOUND")
+        );
+    }
+}
