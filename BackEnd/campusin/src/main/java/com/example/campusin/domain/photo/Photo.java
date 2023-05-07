7311fb194c63b9efea08cbd4e1ebcce2f1a382ce
Kim Tae Ho <kok8454@gmail.com>
2023-05-08 06:05:17 +0900

    Feat. post 관련 photo entity 작성

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/photo/Photo.java
@@ -0,0 +1,41 @@
+package com.example.campusin.domain.photo;
+
+import com.example.campusin.domain.basetime.BaseTimeEntity;
+import com.example.campusin.domain.post.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+/**
+ * Created by kok8454@gmail.com on 2023-05-07
+ * Github : http://github.com/perArdua
+ */
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE photo SET deleted_at = CURRENT_TIMESTAMP where id = ?")
+@Getter
+@NoArgsConstructor
+@Entity
+public class Photo extends BaseTimeEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "photo_id")
+    private Long id;
+
+    @Column(name = "path", nullable = false, length = 300)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", referencedColumnName = "post_id")
+    private Post post;
+
+    public void setPost(final Post post) {
+        this.post = post;
+    }
+
+    public Photo(String content) {
+        this.content = content;
+    }
+}
