1e90717c5b66179d23d6c51bf7b2ac3a299d2f81
Jung So Young <soyoung5611@gmail.com>
2023-05-08 02:00:29 +0900

    Feat. Comment Repository 초안 작성

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/infra/comment/CommentRepository.java
@@ -0,0 +1,17 @@
+package com.example.campusin.infra.comment;
+
+import com.example.campusin.domain.comment.Comment;
+import com.example.campusin.domain.comment.CommentsOnPostResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by kok8454@gmail.com on 2023-03-19
+ * Github : http://github.com/perArdua
+ */
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<CommentsOnPostResponse> findByPost(Long postId, Pageable pageable);
+}
