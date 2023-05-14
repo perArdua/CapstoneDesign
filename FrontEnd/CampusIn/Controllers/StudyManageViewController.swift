f60cc0fa1c58666f85ec316be5368d6cfa21ef2e
ekrud99 <201810999@sangmyung.kr>
2023-05-15 03:04:09 +0900

    Feat. 탭바 연결

---
+++ FrontEnd/CampusIn/Controllers/StudyManageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudyManageViewController.swift
+//  CampusIn
+//
+//  Created by 최다경 on 2023/05/15.
+//
+
+import UIKit
+
+class StudyManageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
