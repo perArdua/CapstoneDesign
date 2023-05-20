201760ff2f4af31c89f49d3f8cb31298c553798c
Jung So Young <soyoung5611@gmail.com>
2023-05-20 17:27:25 +0900

    Feat. Message Api reference model 작성

--- BackEnd/campusin/src/main/java/com/example/campusin/domain/message/dto/request/MessageSendRequest.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/message/dto/request/MessageSendRequest.java
@@ -1,6 +1,9 @@
 package com.example.campusin.domain.message.dto.request;
 
 import javax.validation.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ApiModel(value = "쪽지 전송 요청", description = "전송할 쪽지 내용")
 public class MessageSendRequest {
 
+    @ApiModelProperty(value = "전송할 쪽지 내용", example = "message content")
     @NotBlank(message = "전송할 쪽지 내용은 필수값입니다.")
     private String message;
 
