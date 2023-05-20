378a76c641ea96dbdf741f682bb17c8ab3eecdf0
Jung So Young <soyoung5611@gmail.com>
2023-05-20 21:54:36 +0900

    Feat. Todo Repository 작성

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/infra/todo/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.example.campusin.infra.todo;
+
+import com.example.campusin.domain.todo.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
