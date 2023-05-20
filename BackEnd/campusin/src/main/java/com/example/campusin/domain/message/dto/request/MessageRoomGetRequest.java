3d1ed00e24cab7e9532d51d3ab57b2e36f6a20f5
Kim Tae Ho <kok8454@gmail.com>
2023-05-21 06:13:37 +0900

    Feat. 쪽지응답 관련 api reference 추가

--- BackEnd/campusin/src/main/java/com/example/campusin/domain/message/dto/request/MessageRoomGetRequest.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/message/dto/request/MessageRoomGetRequest.java
@@ -1,6 +1,8 @@
 package com.example.campusin.domain.message.dto.request;
 
 import javax.validation.constraints.NotNull;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Schema(name = "쪽지방 조회 요청", description = "쪽지방 id 입력")
+
 public class MessageRoomGetRequest {
+
+    @Schema(name = "쪽지방 id", example = "1")
     @NotNull(message = "쪽지방 id는 필수값입니다.")
     private Long messageRoomId;
 
--- BackEnd/campusin/src/main/java/com/example/campusin/domain/message/dto/response/MessageResponse.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/message/dto/response/MessageResponse.java
@@ -2,15 +2,22 @@ package com.example.campusin.domain.message.dto.response;
 
 import com.example.campusin.domain.message.Message;
 import com.example.campusin.domain.user.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Schema(name = "쪽지 조회 응답", description = "쪽지를 받은 사람인지, 쪽지 생성 시간, 쪽지 내용")
 public class MessageResponse {
 
+    @Schema(name = "쪽지를 받은 사람인지", example = "true")
     private final Boolean isReceived;
+
+    @Schema(name = "쪽지 생성 시간", example = "2023-05-21 00:00:00")
     private final LocalDateTime createdAt;
+
+    @Schema(name = "쪽지 내용", example = "message content")
     private final String content;
 
     public MessageResponse(Message message, User interlocutor) {
