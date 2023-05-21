ad98e6331f6eed544f4a6c3247f4c7ddca0070b5
taipaise <taipaise@gmail.com>
2023-05-22 04:13:49 +0900

    Feat .자유게시판 게시글 상세 수정

---
+++ FrontEnd/CampusIn/Models/Datas/Posting/GeneralPosting/PostDetail.swift
@@ -0,0 +1,58 @@
+//
+//  PostDetail.swift
+//  CampusIn
+//
+//  Created by 이동현 on 2023/05/22.
+//
+
+import Foundation
+
+// MARK: - Dataresponse
+struct PostDetail: Codable {
+    let header: PostDetailHeader
+    let body: PostDetailBody
+}
+
+// MARK: - Body
+struct PostDetailBody: Codable {
+    let postDetailContent: PostDetailContent
+
+    enum CodingKeys: String, CodingKey {
+        case postDetailContent = "게시글 상세"
+    }
+}
+
+// MARK: - 게시글상세
+struct PostDetailContent: Codable {
+    let postID: Int
+    let boardType: String
+    let userID : Int?
+    let nickname: String?
+    let title, content: String
+    let photoList: [PostDetailPhotoList]
+    let createdAt: [Int]
+
+    enum CodingKeys: String, CodingKey {
+        case postID = "postId"
+        case boardType
+        case userID = "userId"
+        case nickname, title, content, photoList, createdAt
+    }
+}
+
+// MARK: - PhotoList
+struct PostDetailPhotoList: Codable {
+    let photoID: Int
+    let content: String
+
+    enum CodingKeys: String, CodingKey {
+        case photoID = "photoId"
+        case content
+    }
+}
+
+// MARK: - Header
+struct PostDetailHeader: Codable {
+    let code: Int
+    let message: String
+}
