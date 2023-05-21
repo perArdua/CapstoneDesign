d3e293ffe3eecd9e8b61e60ab986dc5d62efcd14
Kim Tae Ho <kok8454@gmail.com>
2023-05-21 19:28:15 +0900

    Feat. 타이머 repository 생성

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/infra/timer/TimerRepository.java
@@ -0,0 +1,11 @@
+package com.example.campusin.infra.timer;
+
+import com.example.campusin.domain.timer.Timer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by kok8454@gmail.com on 2023-05-21
+ * Github : http://github.com/perArdua
+ */
+public interface TimerRepository extends JpaRepository<Timer, Long> {
+}
