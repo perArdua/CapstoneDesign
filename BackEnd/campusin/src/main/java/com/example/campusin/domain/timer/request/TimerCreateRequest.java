1c0d5a1ba2e658e2907db09f267b42bee92db8da
Kim Tae Ho <kok8454@gmail.com>
2023-05-21 20:30:10 +0900

    Feat timer reference 작성

--- BackEnd/campusin/src/main/java/com/example/campusin/domain/timer/request/TimerCreateRequest.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/timer/request/TimerCreateRequest.java
@@ -1,5 +1,7 @@
 package com.example.campusin.domain.timer.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +14,10 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@ApiModel(value = "TimerCreateRequest", description = "타이머 생성 요청")
 public class TimerCreateRequest {
 
+    @ApiModelProperty(value = "과목명", required = true, example = "자료구조")
     private String subject;
 
     @Builder
