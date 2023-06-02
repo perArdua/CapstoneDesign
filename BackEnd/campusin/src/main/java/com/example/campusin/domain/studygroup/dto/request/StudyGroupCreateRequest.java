16118c1dbb8bee76d31b1eb938aec148258d3e59
Jung So Young <soyoung5611@gmail.com>
2023-06-03 04:35:02 +0900

    Feat. StudyGroup dto 수정

--- BackEnd/campusin/src/main/java/com/example/campusin/domain/studygroup/dto/request/StudyGroupCreateRequest.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/studygroup/dto/request/StudyGroupCreateRequest.java
@@ -1,5 +1,7 @@
 package com.example.campusin.domain.studygroup.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +12,13 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 @NoArgsConstructor
+@ApiModel(value = "StudyGroup 생성 요청", description = "StudyGroup name, StudyGroup limitedMemberSize")
 public class StudyGroupCreateRequest {
     @NotNull
+    @ApiModelProperty(value = "StudyGroup name")
     private String studygroupName;
     @NotNull
+    @ApiModelProperty(value = "StudyGroup limitedMemberSize")
     private int LimitedMemberSize;
-
-    private Long userId;
-
 
 }
