d8a8a7e6c8cb7d64643fb4793f25f4332179046d
Kim Tae Ho <kok8454@gmail.com>
2023-06-02 23:04:52 +0900

    Feat. 통계 api reference 작성

--- BackEnd/campusin/src/main/java/com/example/campusin/api/statistics/StatisticsController.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/api/statistics/StatisticsController.java
@@ -4,7 +4,11 @@ import com.example.campusin.application.statistics.StatisticsService;
 import com.example.campusin.common.response.ApiResponse;
 import com.example.campusin.domain.oauth.UserPrincipal;
 import com.example.campusin.domain.statistics.dto.request.StatisticsCreateRequest;
+import com.example.campusin.domain.statistics.dto.response.StatisticsIdResponse;
+import com.example.campusin.domain.statistics.dto.response.StatisticsResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +21,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/statistics")
+@Api(tags = {"통계 API"})
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
 
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "성공", response = StatisticsIdResponse.class)
+            }
+    )
+    @Operation(summary = "통계 생성", description = "특정 날짜에 통계 생성")
     @PostMapping("/create")
     public ApiResponse createStatistics(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                         @RequestBody StatisticsCreateRequest request) {
         return ApiResponse.success("통계 생성", statisticsService.createStatistics(userPrincipal.getUserId(), request));
     }
 
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "성공", response = StatisticsResponse.class)
+            }
+    )
+    @Operation(summary = "통계 조회", description = "특정날짜의 통계 조회")
     @GetMapping("/{statisticsId}")
     public ApiResponse readStatistics(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                       @PathVariable Long statisticsId) {
