e44d1df70beb48e3cfe000f76f07e26af450fb4f
Dagyeong Choi <dagyeong33@gmail.com>
2023-06-05 01:43:04 +0900

    Feat. 랭킹 페이지 UI 작성

---
+++ FrontEnd/CampusIn/Controllers/Ranking/RankingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RankingViewController.swift
+//  CampusIn
+//
+//  Created by 최다경 on 2023/06/05.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    let arr: [String] = ["4등. 남궁00", "5등. 이00"]
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var bronzePersonLabel: UILabel!
+    @IBOutlet weak var silverPersonLabel: UILabel!
+    @IBOutlet weak var goldPersonLabel: UILabel!
+    @IBOutlet weak var toggleBtn: UIButton!
+    @IBOutlet weak var prevBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prevBtn.setTitle("", for: .normal)
+        nextBtn.setTitle("", for: .normal)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "RankingTableViewCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RankingTableViewCell
+        cell.rankingName.text = arr[indexPath.row]
+        cell.rankingImage.image = UIImage(named: "person")
+        return cell
+    }
+    
+}
