8b12aed0536b76aafaba2163e565eaf7ec983195
Jung So Young <soyoung5611@gmail.com>
2023-08-26 17:36:03 +0900

    Feat. Rank Entity

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/domain/rank/Rank.java
@@ -0,0 +1,83 @@
+package com.example.campusin.domain.rank;
+import com.example.campusin.domain.basetime.BaseTimeEntity;
+import com.example.campusin.domain.statistics.Statistics;
+import com.example.campusin.domain.studygroup.StudyGroup;
+import com.example.campusin.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE rank SET deleted_at = CURRENT_TIMESTAMP where rank_id = ?")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Rank extends BaseTimeEntity{
+
+    @Id
+    @Column(name = "rank_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "study_ranking")
+    private Long Studyranking;
+
+    @Column(name = "question_ranking")
+    private Long QuestionRanking;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @ManyToOne
+    @JoinColumn(name = "statistics_id")
+    private Statistics statistics;
+
+    @ManyToOne
+    @JoinColumn(name = "study_group_id")
+    private StudyGroup studyGroup;
+
+    @Column(name = "total_elapsed_time")
+    private Long totalElapsedTime;
+
+    @Column(name = "total_number_of_questions")
+    private Long totalNumberOfQuestions;
+
+    @Column(name = "week")
+    private int week;
+
+    @Builder
+    public Rank(Long Studyranking, Long QuestionRanking, User user, String userName, Statistics statistics, StudyGroup studyGroup, Long totalElapsedTime, Long totalNumberOfQuestions, int week) {
+        this.Studyranking = Studyranking;
+        this.QuestionRanking = QuestionRanking;
+        this.user = user;
+        this.userName = userName;
+        this.statistics = statistics;
+        this.studyGroup = studyGroup;
+        this.totalElapsedTime = totalElapsedTime;
+        this.totalNumberOfQuestions = totalNumberOfQuestions;
+        this.week = week;
+    }
+
+    public void updateStudyRanking(Long ranking) {
+        this.Studyranking = ranking;
+    }
+    public void updateQuestionRanking(Long ranking) {
+        this.QuestionRanking = ranking;
+    }
+
+    public void updateTotalElapsedTime(Long elapsedTime) {
+        this.totalElapsedTime = elapsedTime;
+    }
+
+    public void updateTotalNumberOfQuestions(Long numberOfQuestions) {
+        this.totalNumberOfQuestions = numberOfQuestions;
+    }
+}
