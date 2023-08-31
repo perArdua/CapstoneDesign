1b05c64332e322f36c893b73e52a1c5be2aa4a39
Jung So Young <soyoung5611@gmail.com>
2023-09-01 02:22:58 +0900

    Feat. Rank StudyGroup CRU

--- BackEnd/campusin/src/main/java/com/example/campusin/api/rank/RankController.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/api/rank/RankController.java
@@ -4,12 +4,16 @@ import com.example.campusin.application.rank.RankService;
 import com.example.campusin.common.response.ApiResponse;
 import com.example.campusin.domain.oauth.UserPrincipal;
 import com.example.campusin.domain.rank.dto.request.RankCreateRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = {"RANKING API"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/rank")
@@ -17,39 +21,76 @@ public class RankController {
 
     private final RankService rankService;
 
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "Ranking Id 생성 성공")
+            }
+    )
+    @Operation(summary = "개인 User Rank Create")
     @PostMapping
     public ApiResponse createRank(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                   @RequestBody RankCreateRequest request ){
         return ApiResponse.success("랭킹 생성", rankService.createRank(userPrincipal.getUserId(), request));
     }
 
-//    @PostMapping("/studyGroup/{studyGroupId}")
-//    public ApiResponse createStudyGroupRanking(@PathVariable Long studyGroupId,
-//                                            @RequestBody RankCreateRequest request ){
-//        return ApiResponse.success("스터디 그룹 랭킹 생성", rankService.createStudyGroupRanking(studyGroupId, request));
-//    }
-
-    @GetMapping("/studyRank/{rankId}")
-    public ApiResponse getPersonalStudyTimeRank(@PathVariable Long rankId,
-                                                @AuthenticationPrincipal UserPrincipal userPrincipal){
-        return ApiResponse.success("개인 공부시간 랭킹 조회", rankService.getPersonalStudyTimeRank(userPrincipal.getUserId(), rankId));
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "Ranking Id 생성 성공")
+            }
+    )
+    @Operation(summary = "스터디 그룹 Rank Create")
+    @PostMapping("/{StudyGroupId}")
+    public ApiResponse createStudyGroupRank(@AuthenticationPrincipal UserPrincipal userPrincipal,
+                                            @PathVariable Long StudyGroupId,
+                                            @RequestBody RankCreateRequest request ){
+        return ApiResponse.success("스터디 그룹 랭킹 생성", rankService.createStudyRank(userPrincipal.getUserId(), StudyGroupId, request));
     }
-
-    @GetMapping("/questionRank/{rankId}")
-    public ApiResponse getPersonalQuestionRank(@PathVariable Long rankId,
-                                               @AuthenticationPrincipal UserPrincipal userPrincipal){
-        return ApiResponse.success("개인 질문 랭킹 조회", rankService.getPersonalQuestionRank(userPrincipal.getUserId(), rankId));
-    }
-
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "스터디그룹내 공부시간 랭킹 리스트 조회")
+            }
+    )
+    @Operation(summary = "스터디그룹 내 공부시간 랭킹 리스트 조회")
     @GetMapping("/studyGroupRank/{studyGroupId}")
-    public ApiResponse getStudyGroupRank(@PathVariable Long rankId,
-                                         @AuthenticationPrincipal UserPrincipal userPrincipal){
-        return ApiResponse.success("스터디 그룹 랭킹 조회", rankService.getStudyGroupPersonalStudyTimeRank(userPrincipal.getUserId(), rankId));
+    public ApiResponse getStudyGroupRank(
+                                         @PathVariable Long studyGroupId,
+                                         @AuthenticationPrincipal UserPrincipal userPrincipal,
+                                         @PageableDefault(size = 10) Pageable pageable){
+        return ApiResponse.success("스터디 그룹 랭킹 조회", rankService.getStudyGroupPersonalStudyTimeRank(studyGroupId, pageable));
     }
 
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "공부시간 랭킹 리스트 조회")
+            }
+    )
+    @Operation(summary = "개인 공부시간 랭킹 리스트 조회")
     @GetMapping("/studyTimeRank")
     public ApiResponse getAllStudyTimeRankList(Pageable pageable){
         return ApiResponse.success("공부시간 랭킹 리스트 조회", rankService.getAllStudyTimeRankList(pageable));
+    }
+
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "이전 주차 공부시간 랭킹 리스트 조회")
+            }
+    )
+    @Operation(summary = "이전 주차 Rank 공부시간 순위 리스트 조회")
+    @GetMapping("/{LastWeek}")
+    public ApiResponse getPreviousWeekRankList(@PathVariable int LastWeek,
+                                               @PageableDefault(size = 10) Pageable pageable){
+        return ApiResponse.success("이전 주 랭킹 리스트 조회", rankService.getAllStudyTimeRankListByWeek(LastWeek, pageable));
+    }
+
+    @ApiResponses(
+            value = {
+                    @io.swagger.annotations.ApiResponse(code = 200, message = "개인 질의응답 랭킹 리스트 조회")
+            }
+    )
+    @Operation(summary = "개인 질의응답 랭킹 리스트 조회")
+    @GetMapping("/questionRank")
+    public ApiResponse getAllQuestionRankList(Pageable pageable){
+        return ApiResponse.success("개인 질의응답 랭킹 리스트 조회", rankService.getAllQuestionRankList(pageable));
     }
 
 }
--- BackEnd/campusin/src/main/java/com/example/campusin/infra/rank/RankRepository.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/infra/rank/RankRepository.java
@@ -6,27 +6,32 @@ import com.example.campusin.domain.statistics.Statistics;
 import com.example.campusin.domain.studygroup.StudyGroup;
 import com.example.campusin.domain.user.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface RankRepository extends JpaRepository<Rank, Long> {
-
-    @Query(value = "SELECT COUNT(r) FROM Rank r WHERE r.statistics = :statistics AND r.totalElapsedTime > :totalStudyTime")
-    Long countByStatisticsAndTotalStudyTimeGreaterThan(Statistics statistics, Long totalStudyTime);
-
-    @Query(value = "SELECT COUNT(r) FROM Rank r WHERE r.statistics = :statistics AND r.totalNumberOfQuestions > :totalQuestion")
-    Long countByStatisticsAndTotalQuestionGreaterThan(Statistics statistics, Long totalQuestion);
-
-    @Query(value = "SELECT COUNT(r) FROM Rank r WHERE r.statistics = :statistics AND r.totalElapsedTime > :totalStudyTime AND r.studyGroup = :studyGroup")
-    Long countByStatisticsAndTotalStudyTimeGreaterThanInStudyGroup(StudyGroup studyGroup, Statistics statistics, Long totalStudyTime);
+    @Query(value = "SELECT r FROM Rank r WHERE r.studyGroup.id = :studyGroup order by r.totalElapsedTime desc")
+    Page<Rank> countInStudyGroup(Long studyGroup, Pageable pageable);
 
     @Query(value = "select r from Rank r where r.user = :user and r.statistics = :statistics")
     Rank findByUserAndStatistics(User user, Statistics statistics);
 
-    @Query(value = "select r from Rank r where r.statistics = :statistics and r.studyGroup = :studyGroup")
-    StudyGroup findByStudyGroupAndStatistics(StudyGroup studyGroup, Statistics statistics);
+    @Query("SELECT r FROM Rank r WHERE r.week = :week")
+    Page<Rank> findAllByWeek(int week, Pageable pageable);
+
+    @Query(value = "select r from Rank r where r.user = :user and r.statistics = :statistics and r.studyGroup.id = :studyGroupId")
+    Rank findByUserAndStatisticsAndStudyGroup(User user, Statistics statistics, Long studyGroupId);
+
+    @Query(value = "select r from Rank r  where r.studyGroup.id = null order by r.totalElapsedTime desc")
+    Page<Rank> findAllByOrderByTotalStudyTimeAsc(Pageable pageable);
+
+    @Query(value = "select r from Rank r order by r.totalNumberOfQuestions desc")
+    Page<Rank> findAllByOrderByTotalNumberOfQuestionsAsc(Pageable pageable);
 }
