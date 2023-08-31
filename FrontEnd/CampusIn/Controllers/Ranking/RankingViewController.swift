0f889db7aa82c9105733b3946c1346fcab40363a
taipaise <taipaise@gmail.com>
2023-09-01 00:56:46 +0900

    [CCP-181] Feat. 개인 공부시간 랭킹 API 연결

--- FrontEnd/CampusIn/Controllers/Ranking/RankingViewController.swift
+++ FrontEnd/CampusIn/Controllers/Ranking/RankingViewController.swift
@@ -11,6 +11,9 @@ class RankingViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     
     let arr: [String] = ["4등. 남궁00", "5등. 이00"]
+    var rankingID: Int?
+    var rankingList: [RankingContent] = []
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var bronzePersonLabel: UILabel!
     @IBOutlet weak var silverPersonLabel: UILabel!
@@ -22,18 +25,90 @@ class RankingViewController: UIViewController, UITableViewDelegate, UITableViewD
         super.viewDidLoad()
         prevBtn.setTitle("", for: .normal)
         nextBtn.setTitle("", for: .normal)
+        
+        goldPersonLabel.text = "-"
+        silverPersonLabel.text = "-"
+        bronzePersonLabel.text = "-"
+        
         tableView.delegate = self
         tableView.dataSource = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        RankingManager.createRanking { [self] res in
+            switch res{
+            case .success(let id):
+                rankingID = id
+            case .failure(let err):
+                print(err)
+            }
+        }
+        
+        getCurPersonalStudyRank()
+    }
+    
+    // MARK: - 랭킹 조회 함수
+    //현재 주차 개인 공부 시간 랭킹 조회
+    func getCurPersonalStudyRank(){
+        RankingManager.getPersonalStudyRanking { res in
+            switch res{
+            case .success(let data):
+                self.rankingList = data
+                self.setRanking()
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    //이전 주차 개인 공부 시간 랭킹 조회
+    func getPrevPersonalStudyRank(){
+
+    }
+    
+    //현재 주차 개인 질의 응답 랭킹 조회
+    func getCurPersonalQuesRank(){
+        
+    }
+    
+    //이전 주차 개인 질의 응답 랭킹 조회
+    func getPrevPersonalQuesRank(){
+        
+    }
+    
+    //이전 주차 그룹 공부 시간 랭킹 조회
+    func getPrevGroupStudyRank(){
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 7
+    }
+    
+    func setRanking(){
+        if rankingList.count > 1{
+            goldPersonLabel.text = rankingList[0].name
+        }
+        if rankingList.count > 2{
+            silverPersonLabel.text = rankingList[1].name
+        }
+        if rankingList.count > 3{
+            bronzePersonLabel.text = rankingList[2].name
+        }
+        
+        tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "RankingTableViewCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RankingTableViewCell
-        cell.rankingName.text = arr[indexPath.row]
+        
+        if (indexPath.row + 1) + 3 > rankingList.count{
+            cell.rankingName.text = rankingList[indexPath.row + 3].name
+        }
+        
         cell.rankingImage.image = UIImage(named: "person")
         return cell
     }
@@ -42,5 +117,4 @@ class RankingViewController: UIViewController, UITableViewDelegate, UITableViewD
         print("tap~!@!~#~!#@!~!@#~!#~!@#~!@#~@!#!#~!@#")
         print(")(*&(*7298&$298742974298470ㅂ98749187491827")
     }
-    
 }
