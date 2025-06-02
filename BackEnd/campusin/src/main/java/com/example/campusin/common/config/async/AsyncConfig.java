219c8eb9ce689b9eafd1b8067774c2c8d3a2cecb
Kim Tae Ho <kok8454@gmail.com>
2025-06-03 03:49:34 +0900

    feat: 비동기 실행을 위한 config 작성

---
+++ BackEnd/campusin/src/main/java/com/example/campusin/common/config/async/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.example.campusin.common.config.async;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
