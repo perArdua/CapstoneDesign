c6ed29eb4b3073918dec959f2cef823a54bdc095
Kim Tae Ho <kok8454@gmail.com>
2025-06-03 05:19:19 +0900

    refactor: 누락된 쿼리문 추가

--- BackEnd/campusin/src/main/java/com/example/campusin/application/post/PostService.java
+++ BackEnd/campusin/src/main/java/com/example/campusin/application/post/PostService.java
@@ -1,6 +1,6 @@
 package com.example.campusin.application.post;
 
-import com.example.campusin.application.postSearch.PostSearchIndexer;
+import com.example.campusin.application.postsearch.PostSearchIndexer;
 import com.example.campusin.domain.board.Board;
 import com.example.campusin.domain.board.BoardType;
 import com.example.campusin.domain.board.dto.response.BoardSimpleResponse;
